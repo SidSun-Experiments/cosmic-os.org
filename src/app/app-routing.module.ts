9e0904f66f5b564b0615055b02d3dc8676ec65b0
Sid-Sun <sid-sun@outlook.com>
2019-02-20 00:52:00 +0530

    Add basic components, set-up routing.
    Signed-off-by: Sid-Sun <sid-sun@outlook.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProjectsComponent} from './projects/projects.component';
+import {HomeComponent} from './home/home.component';
+import {SocialComponent} from './social/social.component';
+import {SkillsComponent} from './skills/skills.component';
+import {NotfoundComponent} from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'skills', component: SkillsComponent
+},
+  {
+    path: 'socials', component: SocialComponent
+  },
+  {
+    path: 'projects', component: ProjectsComponent
+  },
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'home', redirectTo: '', pathMatch: 'full'
+  },
+  {
+    path: '**', component: NotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SocialComponent } from './social/social.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    SocialComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
